4f79929d23541a354c340da77508e57199af93dd
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2020-04-13 10:36:18 +0700

    Android Material Design - ToolBar With Menus and OnClick https://www.youtube.com/watch?v=T8yuna4JjfU

---
+++ app/src/main/java/com/example/materialdesign/Main3Activity.java
@@ -0,0 +1,71 @@
+package com.example.materialdesign;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Main3Activity extends AppCompatActivity {
+
+    Toolbar toolbar = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    // make method and implemented on onOptionsItemSelected
+    private void showMessage(String msg){
+        Snackbar.make(toolbar, msg, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //setting xml in menu->menu_main.xml, when clicked show Sncakbar text
+//            Snackbar.make(toolbar,"Settings Clicked",Snackbar.LENGTH_SHORT).show();
+            showMessage("Setting Selected");
+            return true;
+        } else if(id == R.id.contactUs){
+            showMessage("Contact Us Selected");
+            return true;
+        } else {
+            showMessage("About Us Selected");
+            return true;
+        }
+
+        //return super.onOptionsItemSelected(item); //we are not using this
+    }
+
+}
